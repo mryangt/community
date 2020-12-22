a44fb58a248c1a40db90d868293d66f050113c47
yangtao <644330435@qq.com>
2020-12-22 09:21:39 +0800

    20201222

---
+++ src/main/java/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,36 @@
+package majiang.community.controller;
+
+import majiang.community.dto.AccessTokenDTO;
+import majiang.community.dto.GithubUser;
+import majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author YANGTAO
+ * @date 2020/12/20 16:05
+ * Description:
+ */
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("Iv1.2ee9dde46eda16d0");
+        accessTokenDTO.setClient_secret("cc278eca6912efd6203ed92eb7057eac45832dea");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user);
+        return "index";
+    }
+}
